d8dab4ecb80a40f8751fdf48912a5bad09cd8b02
NkitaTmoshin <108579990+NkitaTmoshin@users.noreply.github.com>
2022-09-21 18:53:07 +0300

    Update task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +14,22 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const galeryEl = document.querySelector('.gallery');
+console.log(galeryEl);
+
+
+const elemImgEl = images.map(images => {
+const itemsEl = document.createElement('li');
+itemsEl.classList.add('item');
+console.log(itemsEl);
+
+const imgEl = document.createElement('img');
+imgEl.classList.add('images');
+imgEl.src = images.url;
+imgEl.alt = images.alt;
+console.log(imgEl);
+  return `<li> <img src = '${images.url}', alt = '${images.alt}', width = 150, height = 100> </li>`
+}).join('')
+
+galeryEl.insertAdjacentHTML('afterbegin', elemImgEl);
